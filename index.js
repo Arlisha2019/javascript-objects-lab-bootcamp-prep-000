0079eee28a245cf82fd2510b9da258282f08cf0c
Arlisha2019 <arlisha_hayles@yahoo.com>
2018-08-11 20:32:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   delete newObject.key;
-  return object;
+  return newObject;
 }
 
