5dd7bb57cc528f59b60cd79ff81c39addd0f34e6
Arlisha2019 <arlisha_hayles@yahoo.com>
2018-08-11 19:01:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var recipes = { };
 
 function updateObjectWithKeyAndValue(object, key, value) {
   recipes.key = 'value';
+  
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return recipes.assign({}, recipes, { [key]: 'value'});
+  
 }
